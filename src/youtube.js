411b3da231c63993af84d3e541580c59b3ac4fb7
Ross Libby <rosslibby92@gmail.com>
2025-06-03 17:47:49 -0400

    remove unused code

--- src/youtube.js
+++ src/youtube.js
@@ -1,20 +1,41 @@
 let skipCount = 0;
-const videos = Array.from(document.querySelectorAll('video[src]'));
-const video = videos.find((video) => !video.closest('#video-preview-container'));
-video.addEventListener('timeupdate', (e) => {
-  skipAd();
-});
-video.addEventListener('durationchange', (e) => {
-  skipAd();
-});
 
-function skipAd() {
-  const advertisement = document.querySelector('.video-ads.ytp-ad-module *');
+function isActiveVideo(video) {
+  return !Boolean(video.closest('#video-preview-container'));
+}
 
-  if (advertisement) {
+function getAdOverlay() {
+  const overlay = document.querySelector('[class*=-overlay][class*=-ad-]');
+  return overlay;
+}
+
+function clickSkipButton() {
+  const button = document.querySelector(
+    '[class*=-overlay][class*=-ad-] button:last-child'
+  );
+  button?.click();
+}
+
+function isAdvertisement(div) {
+  if (
+    !(
+      div.closest('[class*=-overlay][class*=-ad-]') ||
+      div.querySelector('[class*=-overlay][class*=-ad-]') ||
+      div.getAttribute('class')?.includes('-ad-')
+    )
+  ) {
+    return false;
+  }
+  return true;
+}
+
+function skipAd(video) {
+  const overlay = getAdOverlay();
+
+  if (overlay) {
     if (isNaN(video.currentTime)) {
-      const skipButton = advertisement.querySelector('.ytp-skip-ad-button');
-      skipButton?.click();
+      const button = overlay.querySelector('button:last-child');
+      button?.click();
     } else {
       ++skipCount;
       console.log(`🔥 Skipping ad ${skipCount} from ${video.currentTime} to ${video.duration}`);
@@ -22,3 +43,75 @@ function skipAd() {
     }
   }
 }
+
+function initializeSkipUtil(video) {
+  const ultraSkipMaster6000 = () => skipAd(video);
+  /**
+   * make sure the util is not already running
+   */
+  video.addEventListener('timeupdate', ultraSkipMaster6000);
+  video.addEventListener('durationchange', ultraSkipMaster6000);
+}
+
+/**
+ * set up for youtube only at this point
+ */
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    let initialized = false;
+
+    for (const addedNode of mutation.addedNodes) {
+      /**
+       * Check if the added node is our new
+       * active video
+       */
+      if (addedNode.nodeName === 'VIDEO') {
+        if (isActiveVideo(addedNode)) {
+          initializeSkipUtil(addedNode);
+          initialized = true;
+          break;
+        }
+      } else if (addedNode.nodeName === 'DIV') {
+        if (isAdvertisement(addedNode)) {
+          clickSkipButton();
+          break;
+        }
+      } else if (addedNode.nodeName === 'BUTTON') {
+        if (
+          addedNode.closest('[class*=-ad-]') &&
+          addedNode.textContent.includes('Skip')
+        ) {
+          addedNode.click();
+          break;
+        }
+      }
+    }
+
+    if (
+      !initialized &&
+      mutation.type === 'attributes' &&
+      mutation.target.nodeName === 'VIDEO' &&
+      mutation.attributeName === 'src'
+    ) {
+      /**
+       * Check if the video source has changed
+       * and if the new video is active
+       */
+      if (isActiveVideo(mutation.target)) {
+        initializeSkipUtil(mutation.target);
+        initialized = true;
+        break;
+      }
+    }
+
+    if (initialized) {
+      break;
+    }
+  }
+});
+
+observer.observe(document.body, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
