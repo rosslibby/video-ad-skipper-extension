e7173cb937ea5a7f72b34890cedadb6b759e38d0
Ross Libby <rosslibby92@gmail.com>
2025-08-24 14:07:52 -0400

    simplified & solid

--- src/pandora.js
+++ src/pandora.js
@@ -1,25 +1,18 @@
+const state = {
+  audioNodes: {},
+  history: [],
+};
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (['childList', 'subtree'].includes(mutation.type)) {
       for (const addedNode of mutation.addedNodes) {
         if (addedNode.nodeName === 'AUDIO') {
           const audio = addedNode;
-          console.log(`New audio node:`, audio, audio.duration, audio.muted);
+          const audioId = addAudioNode(audio);
+          audio.addEventListener('ended', () => removeAudioNode(audioId))
 
           if (audio.src.includes('.adswizz.com')) {
-            audio.playbackRate = 16;
-            audio.addEventListener('loadedmetadata', () => {
-              console.log(`[AU.META]`, audio.currentTime, audio.duration, audio.muted)
-              audio.currentTime = audio.duration - 1;
-            });
-            audio.addEventListener('playing', () => {
-              console.log(`[AU.PLAYING]`, audio.muted, audio.duration, audio.currentTime)
-              audio.pause();
-              setTimeout(() => {
-                audio.currentTime = audio.duration - 1;
-                audio.play();
-              });
-            });
+            audio.addEventListener('playing', handleAdPlaying);
           }
         }
       }
@@ -30,3 +23,51 @@ observer.observe(document.body, {
   childList: true,
   subtree: true,
 });
+
+function addAudioNode(node) {
+  const audioId = crypto.randomUUID();
+  node.setAttribute('data-audio-id', audioId);
+  state.audioNodes[audioId] = {
+    id: audioId,
+    src: node.src,
+    ad: node.src.includes('.adswizz.com'),
+  };
+  console.log(`New audio node ${audioId}:`, node);
+  return audioId;
+}
+
+function removeAudioNode(audioId) {
+  const { ad, src } = state.audioNodes[audioId];
+  const prefix = ad ? 'ad:' : '';
+  state.history.push(`${prefix} ${src}`);
+  delete state.audioNodes[audioId];
+}
+
+function handleAudioTimeUpdate(audio) {
+  if (audio.paused && audio.dataset.skipped) {
+    audio.play();
+  }
+}
+
+function handleAdPlaying(e) {
+  console.log(`⏰ time to skip an ad`);
+  const ad = e.target;
+  ad.pause();
+  ad.currentTime = ad.duration - 1;
+  ad.playbackRate = 16;
+  ad.removeEventListener('playing', handleAdPlaying);
+  ad.play();
+}
+
+function initialRun() {
+  const audios = Array.from(document.querySelectorAll('audio'))
+    .forEach((audio) => {
+      const isAdvertisement = audio.src.includes('.adswizz.com');
+      if (isAdvertisement) {
+        audio.addEventListener('playing', handleAdPlaying);
+      } else {
+        addAudioNode(audio);
+      }
+    });
+}
+initialRun();
