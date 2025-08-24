3e1c75780a839e03f4d5cbfd0b8cdc774139e77a
Ross Libby <rosslibby92@gmail.com>
2025-08-24 15:09:21 -0400

    store current track info

--- src/pandora.js
+++ src/pandora.js
@@ -1,10 +1,11 @@
 const state = {
+  playing: null,
   audioNodes: {},
   history: [],
 };
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
-    if (['childList', 'subtree'].includes(mutation.type)) {
+    if (['childList'].includes(mutation.type)) {
       for (const addedNode of mutation.addedNodes) {
         if (addedNode.nodeName === 'AUDIO') {
           const audio = addedNode;
@@ -33,7 +34,26 @@ function addAudioNode(node) {
     ad: node.src.includes('.adswizz.com'),
   };
   console.log(`New audio node ${audioId}:`, node);
+  node.addEventListener('playing', handleAudioStarted);
   return audioId;
+}
+
+function handleAudioStarted(e) {
+  const audio = e.target;
+  const audioId = audio.dataset.audioId;
+  audio.removeEventListener('playing', handleAudioStarted);
+  const albumArt = document.querySelector('.nowPlayingTopInfo__artContainer > .nowPlayingTopInfo__artContainer__art img').src;
+  const songName = document.querySelector('[data-qa="playing_track_title"]').textContent;
+  const artistName = document.querySelector('[data-qa="playing_artist_name"]').textContent;
+  const albumName = document.querySelector('[data-qa="playing_album_name"]').textContent;
+  const details = {
+    art: albumArt,
+    name: songName,
+    artist: artistName,
+    album: albumName,
+  };
+  const node = state.audioNodes[audioId] || {};
+  state.audioNodes[audioId] = { ...node, ...details };
 }
 
 function removeAudioNode(audioId) {
@@ -52,11 +72,11 @@ function handleAudioTimeUpdate(audio) {
 }
 
 function handleAdPlaying(e) {
-  console.log(`⏰ time to skip an ad`);
+  console.log(`⚡️ zapping an ad`);
   const ad = e.target;
   ad.volume = 0.01;
   ad.pause();
-  ad.currentTime = ad.duration - 0.1;
+  ad.currentTime = ad.duration - 0.15;
   ad.playbackRate = 16;
   ad.removeEventListener('playing', handleAdPlaying);
   ad.play();
