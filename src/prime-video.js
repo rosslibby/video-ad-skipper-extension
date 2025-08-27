4402c9156e26e92f84d756f3e378d2de58889ec9
Ross Libby <rosslibby92@gmail.com>
2025-08-27 19:39:37 -0400

    remove whitespaces

--- src/prime-video.js
+++ src/prime-video.js
@@ -18,7 +18,7 @@ function primeSetup() {
   const renderer = video.parentElement;
   video.controls = true;
   renderer.style.pointerEvents = 'initial';
-  
+
   function skipAd(seconds) {
     /**
      * Skip by the specified seconds + 1 to ensure the ad's
@@ -30,12 +30,12 @@ function primeSetup() {
     video.muted = false;
     showToast();
   }
-  
+
   const observer = new MutationObserver(() => {
     const timeRemaining = document.querySelector(
       'span[class*=-ad-timer-remaining-time]'
     );
-  
+
     if (timeRemaining) {
       skipCount++;
       const [min = 0, sec = 0] = timeRemaining.textContent.split(':');
@@ -52,14 +52,14 @@ function primeSetup() {
       }, 1000);
     }
   });
-  
+
   observer.observe(container, {
     attributes: false,
     childList: true,
     subtree: true,
     characterData: false,
   });
-  
+
   function setupToast() {
     const styling = `.toast{animation: hidetoast 2.2s ease-in-out;backdrop-filter: blur(4px);background-color: #ffffff1c;border: 1px solid #ffffff1c;border-radius: 4px;box-shadow: 0 0 0 1px #0000000a, 0 0 8px 0 #0000000a inset;box-sizing: border-box;color: #ffffff;font-family: system-ui;font-weight: 200;height: max-content !important;left: 2rem;opacity: 0;padding: 8px 12px;position: absolute;top: 2rem;width: max-content !important;z-index: 9999}
     @keyframes hidetoast {0%{opacity: 0}25%{opacity: 1}50%{opacity: 1}75%{opacity: 1}99%{opacity: 0}100%{opacity: 0}}`;
@@ -68,9 +68,9 @@ function primeSetup() {
     styletag.innerHTML = styling;
     document.head.appendChild(styletag);
   }
-  
+
   setupToast();
-  
+
   function makeToast() {
     const toast = document.createElement('div');
     toast.className = 'toast';
@@ -79,13 +79,13 @@ function primeSetup() {
     toast.innerText = label;
     return toast;
   }
-  
+
   function showToast() {
     const toast = makeToast();
     video.parentElement.appendChild(toast);
     setTimeout(() => toast.remove(), 2201);
   }
-  
+
   document.addEventListener('keypress', (e) => {
     if (e.key === 'p') {
       e.preventDefault();
