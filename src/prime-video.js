6c788824ff9b1941c01f09da61e3690ccbc95da2
Ross Libby <rosslibby92@gmail.com>
2025-10-03 12:20:14 -0400

    massive simplification + it works

--- src/prime-video.js
+++ src/prime-video.js
@@ -1,144 +1,137 @@
-const setupObserver = new MutationObserver(() => {
-  const container = document.querySelector('[id*=dv-web-player]');
-  const video = container?.querySelector('video[src*=blob]');
+let skipCount = 0;
+const state = {
+  timer: false,
+  video: null,
+  container: null,
+  adContainer: null,
+};
 
-  if (video) {
-    primeSetup();
-    setupObserver.disconnect();
+const toggleTimer = () => {
+  state.timer = !state.timer;
+}
+
+const tracker = {
+  video: [],
+  container: [],
+  timer: [],
+};
+
+const trackItem = (key, mutation) => {
+  const payload = {
+    type: mutation.type,
+    prev: mutation.oldValue,
+    current: mutation.target.outerHTML,
+    time: Date.now(),
+  };
+  tracker[key].push(payload);
+};
+
+function setupToast() {
+  const styling = `.toast{animation: hidetoast 2.2s ease-in-out;backdrop-filter: blur(4px);background-color: #ffffff1c;border: 1px solid #ffffff1c;border-radius: 4px;box-shadow: 0 0 0 1px #0000000a, 0 0 8px 0 #0000000a inset;box-sizing: border-box;color: #ffffff;font-family: system-ui;font-weight: 200;height: max-content !important;left: 2rem;opacity: 0;padding: 8px 12px;position: absolute;top: 2rem;width: max-content !important;z-index: 9999}
+  @keyframes hidetoast {0%{opacity: 0}25%{opacity: 1}50%{opacity: 1}75%{opacity: 1}99%{opacity: 0}100%{opacity: 0}}`;
+  const styletag = document.createElement('style');
+  styletag.setAttribute('type', 'text/css');
+  styletag.innerHTML = styling;
+  document.head.appendChild(styletag);
+}
+
+setupToast();
+
+function makeToast() {
+  const toast = document.createElement('div');
+  toast.className = 'toast';
+  toast.innerText = 'Skipped ad';
+  const label = `Skipped ad #${skipCount}`;
+  toast.innerText = label;
+  return toast;
+}
+
+function showToast() {
+  const video = document.querySelector('video[src*=blob]');
+  const toast = makeToast();
+  video.parentElement.appendChild(toast);
+  setTimeout(() => toast.remove(), 2201);
+}
+
+async function skipSegment(seconds) {
+  skipCount++;
+  console.log(`[${skipCount}] Skipping ahead by ${seconds} seconds`);
+  showToast();
+  document.querySelector('video[src*=blob]').currentTime += seconds;
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), 100);
+  })
+}
+
+async function skip(timer) {
+  if (!timer.textContent) return;
+  const seconds = timer.textContent
+    .match(/(\d+):(\d+)/g)[0]
+    .split(':').map(Number)
+    .reduce((acc, n, i) => {
+      if (i === 0) return acc + n * 60;
+      return acc + n;
+    }, 0);
+  const increments = Math.ceil(seconds / 30);
+  const remainder = seconds % 30;
+  const times = Array.from({ length: increments }, (_, i) => {
+    if (i < increments - 1) return 30;
+    return remainder;
+  });
+  for (const t of times) {
+    await skipSegment(t);
+    if (state.timer && t < 30) {
+      toggleTimer();
+    }
   }
+}
+
+const adContainerObserver = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === 'childList') {
+      console.log(`⏰ Ad timer detected:`, mutation.target);
+      skip(mutation.target);
+    } else {
+      console.log(mutation.type, mutation.target.textContent)
+    }
+  })
 });
-setupObserver.observe(document.body, {
+
+const setContainers = (video) => {
+  if (!state.container && !state.adContainer) {
+    const container = video.closest('[id*=dv-web-player]');
+    const adContainer = document.querySelector('.atvwebplayersdk-ad-timer-countdown');
+    state.container = container;
+    state.adContainer = adContainer;
+    adContainerObserver.observe(adContainer, {
+      childList: true,
+      attributes: true,
+      characterData: true,
+    })
+  } else {
+  }
+}
+
+function videoPlaying(e) {
+  const video = e.target;
+  setContainers(video);
+}
+
+console.clear();
+const initobserver = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === 'attributes' || mutation.type == 'characterData') {
+      if (mutation.target.nodeName === 'VIDEO') {
+        const video = mutation.target;
+        trackItem('video', mutation);
+        video.addEventListener('play', videoPlaying);
+      }
+    }
+  });
+});
+initobserver.observe(document.body, {
+  attributes: true,
+  characterData: true,
   childList: true,
   subtree: true,
 });
-function primeSetup() {
-  let skipCount = 0;
-  let observing = false;
-  let keyboard = false;
-  const container = document.querySelector('[id*=dv-web-player]');
-  const video = container.querySelector('video[src*=blob]');
-  const renderer = video.parentElement;
-  video.controls = true;
-  renderer.style.pointerEvents = 'initial';
-
-  async function skipSegment(seconds) {
-    skipCount++;
-    console.log(`[${skipCount}] Skipping ahead by ${seconds} seconds`);
-    showToast();
-    document.querySelector('video[src*=blob]').currentTime += seconds;
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(), 250);
-    })
-  }
-
-  async function skip() {
-    const timeRemaining = document.querySelector(
-      'span[class*=-ad-timer-remaining-time]'
-    );
-    const [min = 0, sec = 0] = timeRemaining.textContent.split(':');
-    const secondsRemaining = parseInt(min) * 60 + parseInt(sec);
-    const increments = Math.ceil(secondsRemaining / 30);
-    const remainder = secondsRemaining % 30 - 1;
-    const times = Array.from({ length: increments }, (_, i) => {
-      if (i < increments - 1) return 30;
-      return remainder;
-    });
-    for (const t of times) {
-      await skipSegment(t);
-    }
-  }
-
-  const observer = new MutationObserver(() => {
-    const timeRemaining = document.querySelector(
-      'span[class*=-ad-timer-remaining-time]'
-    );
-
-    if (timeRemaining) {
-      skip();
-    }
-  });
-
-  function toggleObserver() {
-    observing = !observing;
-    console.log(`♻️ [${Date.now()}] Recycling observer: ${observing ? 'on' : 'off'}`);
-    if (observing) {
-      observer.observe(container, {
-        attributes: false,
-        childList: true,
-        subtree: true,
-        characterData: false,
-      });
-    } else {
-      observer.disconnect();
-    }
-  }
-
-  video.addEventListener('play', startObservingWhenVideoPlays);
-
-  function startObservingWhenVideoPlays(e) {
-    toggleObserver();
-  }
-
-  function setupToast() {
-    const styling = `.toast{animation: hidetoast 2.2s ease-in-out;backdrop-filter: blur(4px);background-color: #ffffff1c;border: 1px solid #ffffff1c;border-radius: 4px;box-shadow: 0 0 0 1px #0000000a, 0 0 8px 0 #0000000a inset;box-sizing: border-box;color: #ffffff;font-family: system-ui;font-weight: 200;height: max-content !important;left: 2rem;opacity: 0;padding: 8px 12px;position: absolute;top: 2rem;width: max-content !important;z-index: 9999}
-    @keyframes hidetoast {0%{opacity: 0}25%{opacity: 1}50%{opacity: 1}75%{opacity: 1}99%{opacity: 0}100%{opacity: 0}}`;
-    const styletag = document.createElement('style');
-    styletag.setAttribute('type', 'text/css');
-    styletag.innerHTML = styling;
-    document.head.appendChild(styletag);
-  }
-
-  setupToast();
-
-  function makeToast() {
-    const toast = document.createElement('div');
-    toast.className = 'toast';
-    toast.innerText = 'Skipped ad';
-    const label = `Skipped ad #${skipCount}`;
-    toast.innerText = label;
-    return toast;
-  }
-
-  function showToast() {
-    const toast = makeToast();
-    video.parentElement.appendChild(toast);
-    setTimeout(() => toast.remove(), 2201);
-  }
-
-  function handleKeyDown(e) {
-    if (e.key === 'k') {
-      keyboard = !keyboard;
-    } else if (e.key === 'p') {
-      if (document.pictureInPictureElement) {
-        document.exitPictureInPicture();
-      } else {
-        video.requestPictureInPicture();
-      }
-    } else if (e.key === 'o') {
-      toggleObserver();
-    }
-
-    if (keyboard && ['ArrowLeft', 'ArrowRight', ' ', 'f'].includes(e.key)) {
-      e.preventDefault();
-
-      const video = document.querySelector('video[src*=blob]');
-
-      if (e.key === 'ArrowLeft') {
-        video.currentTime -= 10;
-      } else if (e.key === 'ArrowRight') {
-        video.currentTime += 10;
-      } else if (e.key === ' ') {
-        if (video.paused) {
-          video.play();
-        } else {
-          video.pause();
-        }
-      } else if (e.key === 'f') {
-        if (!document.fullscreenElement) {
-          video.requestFullscreen();
-        }
-      }
-    }
-  }
-  document.addEventListener('keydown', handleKeyDown)
-}
